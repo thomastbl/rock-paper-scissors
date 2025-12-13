4bc73189f41c27348613d7bb87214f452d6dda1a
thomastbl <101262386+thomastbl@users.noreply.github.com>
2025-12-13 22:30:37 +0100

    feat: complete game points attribution system

--- javascript.js
+++ javascript.js
@@ -36,24 +36,34 @@ function computerMakeChoice () {
     return computerChoice;
 }
 
-function calcPoints (player1, player2) {
+function initializePoints (player1points, player2points) {
+    return player1points, player2points = 0;
+}
 
+function calcPoints (player) {
+    return ++player;
 }
 
 function calculateRoundWinner (player1choice, player2choice, player1points, player2points) {
     if ((player1choice === "ROCK" && player2choice === "SCISSORS")||(player1choice === "PAPER" && player2choice === "ROCK")||(player1choice === "SCISSORS" && player2choice === "PAPER")) {
         calcPoints(player1points)
+        
     } else if ((player2choice === "ROCK" && player1choice === "SCISSORS")||(player2choice === "PAPER" && player1choice === "ROCK")||(player2choice === "SCISSORS" && player1choice === "PAPER")) {
         calcPoints(player2points)
 } 
 }
 
-function matchRound () {
-    let player1points, player2points = 0;
+function matchRound (playerPoints, computerPoints) {
     let playerChoice = playerMakeChoice();
     let computerChoice = computerMakeChoice();
     console.log(`You : ${playerChoice} | Computer : ${computerChoice}`);
-    let rounds = Number(prompt("Combien de rounds ?"));
+    calculateRoundWinner(playerChoice, computerChoice, playerPoints, computerPoints);
+
 }
 
-matchRound();
+let playerPoints = initializePoints();
+let computerPoints = initializePoints();
+let rounds = Number(prompt("Combien de rounds ?"));
+for (i=0; i<rounds; i++){
+    matchRound(playerPoints, computerPoints);
+}
